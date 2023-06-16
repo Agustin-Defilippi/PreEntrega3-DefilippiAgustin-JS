2719537e0c0565b500013557d96f2a3b6a675cbf
Agustin Defilippi <defilippiagustin@hotmail.com>
2023-06-16 20:51:10 -0300

    desarrollado paquetes de viaje

--- js/main.js
+++ js/main.js
@@ -1,18 +1,98 @@
-const viajes = [{nombre:"Salta la linda",destino:"salta",duracion:"5 dias",boleto:"ida y vuelta",hospedaje:"Andina Hotel",precio:82500,categoria:"oferta",zona:"norte",imagen:"https://i.ytimg.com/vi/ulDUoyVld9E/maxresdefault.jpg"}
+const viajes = [
+    {nombre:"Salta la linda",
+    destino:"salta",
+    duracion:"5 dias",
+    boleto:"ida y vuelta",
+    hospedaje:"Andina Hotel",
+    precio:82500,
+    categoria:"oferta",
+    zona:"norte",
+    imagen:"https://i.ytimg.com/vi/ulDUoyVld9E/maxresdefault.jpg",
+    imagen2:`<img  class="img-thumbnail"  src="img/salta1.jpg" alt="logo de argentina para pagina">`,
+    excursiones: " City Tour en la ciudad de Salta visitando el casco histórico, monumento a Güemes y feria artesanal,Peña de Balderrama, Quebrada de Cafayate, Cachi, San Antonio de los Cobres, Quebrada de Humahuaca y Salinas Grandes",
+    coordinador: "Luciano Diaz",
+    salida: "17 de agosto",
+    slogan : "Salta la ciudad de tus amores con Defilippi Tourlines."
 
-,{nombre:"Formosa",destino:"formosa",duracion:"5 dias",boleto:"ida y vuelta",hospedaje:"Solar del lago cabañas",precio:68900,categoria:"oferta",zona:"norte",imagen:"https://agenfor.com.ar/wp-content/uploads/2019/12/Portada-1.png"}
+    },
 
-,{nombre:"Misiones, Cataratas del Iguazu",destino:"misiones",duracion:"7 dias",boleto:"ida y vuelta",hospedaje:"El salto del tigre",precio:112700,categoria:"alta",zona:"norte",imagen:"https://cdn.getyourguide.com/img/location/56125e97639e0.jpeg/70.jpg"}
-,
-{nombre:"San Luis",destino:"san luis",duracion:"3 dias",boleto:"ida y vuelta",hospedaje:"La chaqueñita cabañas",precio:52000,categoria:"oferta",zona:"centro",imagen:"https://upload.wikimedia.org/wikipedia/commons/4/42/Atardecer_con_el_Cerro_Negro.JPG"}
-,
-{nombre:"Buenos Aires",destino:"buenos aires",duracion:"3 dias",boleto:"ida y vuelta",hospedaje:"Hotel Monaco",precio:39500,categoria:"baja",zona:"centro",imagen:"https://blogskystorage.s3.amazonaws.com/2021/07/skyairline_skyairline_image_182.jpeg"}
-,
-{nombre:"San Juan",destino:"san juan",duracion:"7 dias",boleto:"ida y vuelta",hospedaje:"Temu Hoteles",precio:131700,categoria:"alta",zona:"centro",imagen:"https://i.ytimg.com/vi/x59_AVACZWo/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGEcgYShlMA8=&rs=AOn4CLDvcOS2i3i3d1itVFnIebOFkjxM_Q"}
-,
-{nombre:"Santa Cruz",destino:"santa cruz",duracion:"4 dias",boleto:"ida y vuelta",hospedaje:"Cabañas el Arriero",precio:100000,categoria:"alta",zona:"sur",imagen:"https://upload.wikimedia.org/wikipedia/commons/3/33/C%C2%BA_Fitz_Roy%2C_Chalt%C3%A9n_-_Argentina.jpg"}
-,
-{nombre:"Calafate",destino:"calafate",duracion:"5 dias",boleto:"ida y vuelta",hospedaje:"Suites la Ferrere",precio:185300,categoria:"oferta",zona:"sur",imagen:"https://i.ytimg.com/vi/54DgAZHTAEI/maxresdefault.jpg"}];
+    {nombre:"Formosa",
+    destino:"formosa",
+    duracion:"5 dias",
+    boleto:"ida y vuelta",
+    hospedaje:"Solar del lago cabañas",
+    precio:68900,
+    categoria:"oferta",
+    zona:"norte",
+    imagen:"https://agenfor.com.ar/wp-content/uploads/2019/12/Portada-1.png"
+    },
+
+    {nombre:"Misiones, Cataratas del Iguazu",
+    destino:"misiones",
+    duracion:"7 dias",
+    boleto:"ida y vuelta",
+    hospedaje:"El salto del tigre",
+    precio:112700,
+    categoria:"alta",
+    zona:"norte",
+    imagen:"https://cdn.getyourguide.com/img/location/56125e97639e0.jpeg/70.jpg"
+    },
+    
+    {nombre:"San Luis",
+    destino:"san luis",
+    duracion:"3 dias",
+    boleto:"ida y vuelta",
+    hospedaje:"La chaqueñita cabañas",
+    precio:52000,
+    categoria:"oferta",
+    zona:"centro",
+    imagen:"https://upload.wikimedia.org/wikipedia/commons/4/42/Atardecer_con_el_Cerro_Negro.JPG"
+    },
+
+    {nombre:"Buenos Aires",
+    destino:"buenos aires",
+    duracion:"3 dias",
+    boleto:"ida y vuelta",
+    hospedaje:"Hotel Monaco",
+    precio:39500,
+    categoria:"baja",
+    zona:"centro",
+    imagen:"https://blogskystorage.s3.amazonaws.com/2021/07/skyairline_skyairline_image_182.jpeg"
+    },
+
+    {nombre:"San Juan",
+    destino:"san juan",
+    duracion:"7 dias",
+    boleto:"ida y vuelta",
+    hospedaje:"Temu Hoteles",
+    precio:131700,
+    categoria:"alta",
+    zona:"centro",
+    imagen:"https://i.ytimg.com/vi/x59_AVACZWo/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGEcgYShlMA8=&rs=AOn4CLDvcOS2i3i3d1itVFnIebOFkjxM_Q"
+    },
+
+    {nombre:"Santa Cruz",
+    destino:"santa cruz",
+    duracion:"4 dias",
+    boleto:"ida y vuelta",
+    hospedaje:"Cabañas el Arriero",
+    precio:100000,
+    categoria:"alta",
+    zona:"sur",
+    imagen:"https://upload.wikimedia.org/wikipedia/commons/3/33/C%C2%BA_Fitz_Roy%2C_Chalt%C3%A9n_-_Argentina.jpg"
+    },
+
+    {nombre:"Calafate",
+    destino:"calafate",
+    duracion:"5 dias",
+    boleto:"ida y vuelta",
+    hospedaje:"Suites la Ferrere",
+    precio:185300,
+    categoria:"oferta",
+    zona:"sur",
+    imagen:"https://i.ytimg.com/vi/54DgAZHTAEI/maxresdefault.jpg"
+    }
+];
 
 const guardadoPaquetesViajesLs = () =>{
     localStorage.setItem("viajess",JSON.stringify(viajes));
--- js/paqueteViaje.js
+++ js/paqueteViaje.js
@@ -6,23 +6,61 @@ const viajeFiltrado = () =>{
             <div class="card bg-card css-card border-dark " style="width: 18rem;">
                 <img src="${viajeFiltrado.imagen}" class="card-img-top" alt="${viajeFiltrado.destino}">
                 <div class="card-body">
-                <h4>${viajeFiltrado.nombre} 2023 te está esperando!<br></h4>
-                <p class="card-text">
-                Con Defilippi Tourlines esto es posible, aprovecha con nosotros la posibilidad de poder hacer tu  sueño realidad.<br><br>
-                <b>Precio<b>: $${viajeFiltrado.precio}
-                </p>
+                    <h4>${viajeFiltrado.nombre} 2023 te está esperando!<br></h4>
+                    <p class="card-text">
+                    Con Defilippi Tourlines esto es posible, aprovecha con nosotros la posibilidad de poder hacer tu  sueño realidad.<br><br>
+                    <b>Precio<b>: $${viajeFiltrado.precio}
+                    </p>
+                </div>
+                <div class= "d-flex justify-content-center css-div mb-2">
+                <button id="btn-conoceMas" class="btn bg-btn-render border-dark css-button text-light">Más Información</button>
                 </div>
             </div>
       </div>`;
     
-   
-      
-       
-        
-    
-    
     document.getElementById("paqueteViajeFiltrado").innerHTML=cardViajeFiltrado
-
 }
 
 viajeFiltrado();
+
+const btnConoceMas = document.getElementById("btn-conoceMas");
+
+btnConoceMas.addEventListener("click",() =>{
+    const InfoAdicional = document.getElementById("informacionAdicional");
+    const  infoDestino = JSON.parse(localStorage.getItem("paqueteViajeBusqueda"));
+    
+    let informacion = `
+        <div class="card mb-4 w-100 css-infoViaje" style="width: 18rem;">
+    
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="${infoDestino.imagen}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="${infoDestino.imagen2}" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+          
+            <div class="card-body">
+                <h5 class="card-title text-center">${infoDestino.nombre}</h5>
+                <p class="card-text my-2">${infoDestino.slogan}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><b>Fecha de salida</b>: ${infoDestino.salida}.</li>
+                <li class="list-group-item"><b>Excursiones</b>: ${infoDestino.excursiones}.</li>
+                <li class="list-group-item"><b>Coordinador</b>: ${infoDestino.coordinador}.</li>
+            </ul>
+        </div>
+    `
+    InfoAdicional.innerHTML = informacion;
+});
