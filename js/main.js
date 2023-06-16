39228bbdfe130aced4c41c46b5aa367801e48950
Agustin Defilippi <defilippiagustin@hotmail.com>
2023-06-15 23:35:56 -0300

    agregado de img a paquetes de viajes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const viajes = [{nombre:"Salta la linda",destino:"salta",duracion:"5 dias",boleto:"ida y vuelta",hospedaje:"Andina Hotel",precio:82500,categoria:"oferta",zona:"norte",imagen:"https://i.ytimg.com/vi/ulDUoyVld9E/maxresdefault.jpg"}
+
 ,{nombre:"Formosa",destino:"formosa",duracion:"5 dias",boleto:"ida y vuelta",hospedaje:"Solar del lago cabañas",precio:68900,categoria:"oferta",zona:"norte",imagen:"https://agenfor.com.ar/wp-content/uploads/2019/12/Portada-1.png"}
-,{destino:"misiones",duracion:"7 dias",boleto:"ida y vuelta",hospedaje:"El salto del tigre",precio:112700,categoria:"alta",zona:"norte"},
-{nombre:"San Luis",destino:"san luis",duracion:"3 dias",boleto:"ida y vuelta",hospedaje:"La chaqueñita cabañas",precio:52000,categoria:"oferta",zona:"centro",imagen:"https://upload.wikimedia.org/wikipedia/commons/4/42/Atardecer_con_el_Cerro_Negro.JPG"},
-{destino:"buenos aires",duracion:"3 dias",boleto:"ida y vuelta",hospedaje:"Hotel Monaco",precio:39500,categoria:"baja",zona:"centro"},
-{destino:"san juan",duracion:"7 dias",boleto:"ida y vuelta",hospedaje:"Temu Hoteles",precio:131700,categoria:"alta",zona:"centro"},
-{destino:"santa cruz",duracion:"4 dias",boleto:"ida y vuelta",hospedaje:"Cabañas el Arriero",precio:100000,categoria:"alta",zona:"sur"},
+
+,{nombre:"Misiones, Cataratas del Iguazu",destino:"misiones",duracion:"7 dias",boleto:"ida y vuelta",hospedaje:"El salto del tigre",precio:112700,categoria:"alta",zona:"norte",imagen:"https://cdn.getyourguide.com/img/location/56125e97639e0.jpeg/70.jpg"}
+,
+{nombre:"San Luis",destino:"san luis",duracion:"3 dias",boleto:"ida y vuelta",hospedaje:"La chaqueñita cabañas",precio:52000,categoria:"oferta",zona:"centro",imagen:"https://upload.wikimedia.org/wikipedia/commons/4/42/Atardecer_con_el_Cerro_Negro.JPG"}
+,
+{nombre:"Buenos Aires",destino:"buenos aires",duracion:"3 dias",boleto:"ida y vuelta",hospedaje:"Hotel Monaco",precio:39500,categoria:"baja",zona:"centro",imagen:"https://blogskystorage.s3.amazonaws.com/2021/07/skyairline_skyairline_image_182.jpeg"}
+,
+{nombre:"San Juan",destino:"san juan",duracion:"7 dias",boleto:"ida y vuelta",hospedaje:"Temu Hoteles",precio:131700,categoria:"alta",zona:"centro",imagen:"https://i.ytimg.com/vi/x59_AVACZWo/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGEcgYShlMA8=&rs=AOn4CLDvcOS2i3i3d1itVFnIebOFkjxM_Q"}
+,
+{nombre:"Santa Cruz",destino:"santa cruz",duracion:"4 dias",boleto:"ida y vuelta",hospedaje:"Cabañas el Arriero",precio:100000,categoria:"alta",zona:"sur",imagen:"https://upload.wikimedia.org/wikipedia/commons/3/33/C%C2%BA_Fitz_Roy%2C_Chalt%C3%A9n_-_Argentina.jpg"}
+,
 {nombre:"Calafate",destino:"calafate",duracion:"5 dias",boleto:"ida y vuelta",hospedaje:"Suites la Ferrere",precio:185300,categoria:"oferta",zona:"sur",imagen:"https://i.ytimg.com/vi/54DgAZHTAEI/maxresdefault.jpg"}];
 
 const guardadoPaquetesViajesLs = () =>{
