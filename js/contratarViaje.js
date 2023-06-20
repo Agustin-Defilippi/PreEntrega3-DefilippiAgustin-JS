d915176d2256db8177d26876496782ee0df0ebb9
Agustin Defilippi <defilippiagustin@hotmail.com>
2023-06-19 23:42:44 -0300

    Finalizado PreEntrega-3 DefilippiAgustin

--- js/contratarViaje.js
+++ js/contratarViaje.js
@@ -24,46 +24,28 @@ class PasajeCliente{
         this.nacionalidad = nacionalidad;
     }
 
-    mayorEdad(numero){
-        if(this.dni<numero){
-            alert(`Usted es mayor de edad. \n Debera presentar fotocopia a color de su DNI numero ${this.dni} de ambos lados y su identificacion en forma de física en la agencia.`);
-            
-        }else if(this.dni>numero){
-            alert("Usted es menor de edad,deberá presentar autorizacion de padre, madre y/o tutor, presentarse en la agencia de turismo");
-
+    
+      mayorEdad(numero){
+        if (this.edad < numero) {
+          return "Usted es menor de edad. Deberá presentar autorización de padre, madre y/o tutor y presentarse en la agencia de turismo.";
         }else{
-            alert("ingreso un numero de dni incorrecto.")
+          return `Usted es mayor de edad. Deberá presentar fotocopia a color de su DNI número ${this.dni} de ambos lados y su identificación en forma física en la agencia.`;
         }
-    }
+      } 
+
 
     nacionalidadPasajero(){
-      if(this.nacionalidad === "argentino"){
-        alert("Usted es argentino, ingrese a MI ARGENTINA y descargue el certificado de vacunacion contra COVID-19.");
-      }else if(this.nacionalidad !== "argentino"){
-        alert("Usted es extranjero, por lo tanto debera presentar carnet de vacunacion contra COVID-19, y sera excluyente el aplicado de tres dosis correspondientes.");
+      if(this.nacionalidad === "ARGENTINO"){
+        return"Usted es argentino, ingrese a MI ARGENTINA y descargue el certificado de vacunacion contra COVID-19.";
+      }else if(this.nacionalidad !== "ARGENTINO"){
+        return"Usted es extranjero, por lo tanto debera presentar carnet de vacunacion contra COVID-19, y sera excluyente el aplicado de tres dosis correspondientes.";
       }else{
-        alert("ingreso un dato incorrecto.");
+        return"ingreso un dato incorrecto.";
       }
     }
 }
 
-const nombreUsuario = document.getElementById("nombreUsuario").value.toUpperCase();
-const apellidoUsuario = document.getElementById("apellidoUsuario").value.toUpperCase();
-const edadUsuario = document.getElementById("edadUsuario").value.toUpperCase();
-const dniUsuario = document.getElementById("dniUsuario").value.toUpperCase();
-const correoUsuario = document.getElementById("correoElectronico").value.toUpperCase();
-const provinciaUsuario = document.getElementById("provinciaUsuario").value.toUpperCase();
-const ciudadUsuario = document.getElementById("ciudadUsuario").value.toUpperCase();
-const nacionalidadUsuario = document.getElementById("nacionalidadUsuario").value.toUpperCase();
 
-
-
-
-
-
-const usuario = new PasajeCliente(nombreUsuario,apellidoUsuario,edadUsuario,dniUsuario,correoUsuario,provinciaUsuario,nacionalidadUsuario)
-
-console.log(usuario);
 
 
 
@@ -83,8 +65,8 @@ const formularioUsuario = () =>{
 
   if((nombreUsuario == "") || (apellidoUsuario == "") || (edadUsuario == "") || (dniUsuario == "") || (correoUsuario == "") || (provinciaUsuario == "") || (ciudadUsuario == "") || (nacionalidadUsuario == "")){
 
-    completarCampos.innerHTML= "Error, por favor complete todos los campos de ingreso, para avanzar.";
-    completarCampos.className= "text-danger mb-1"
+    completarCampos.innerHTML= "Error!, por favor complete todos los campos de ingreso, para avanzar.";
+    completarCampos.className= "text-danger text-center mb-1"
     return false;
   }else{
     completarCampos.innerHTML = "";
@@ -101,14 +83,40 @@ const fomularioIngresoUsuario = document.getElementById("fomularioUsuario")
 
 fomularioIngresoUsuario.addEventListener("submit",(e) =>{
   e.preventDefault();
- 
-  const usuarios = usuario;
+
+  const nombreUsuario = document.getElementById("nombreUsuario").value.toUpperCase();
+  const apellidoUsuario = document.getElementById("apellidoUsuario").value.toUpperCase();
+  const edadUsuario = document.getElementById("edadUsuario").value.toUpperCase();
+  const dniUsuario = document.getElementById("dniUsuario").value.toUpperCase();
+  const correoUsuario = document.getElementById("correoElectronico").value.toUpperCase();
+  const provinciaUsuario = document.getElementById("provinciaUsuario").value.toUpperCase();
+  const ciudadUsuario = document.getElementById("ciudadUsuario").value.toUpperCase();
+  const nacionalidadUsuario = document.getElementById("nacionalidadUsuario").value.toUpperCase();
+
   
-  if(formularioUsuario() == true){
-    const pasaporteDestino =JSON.parse(localStorage.getItem("paqueteViajeBusqueda"));
-    localStorage.setItem("datosUsuario",JSON.stringify(usuarios));
-    setTimeout(() => location.href = "pasajeUsuario.html", 1500);
+
+  const usuario = new PasajeCliente(nombreUsuario,apellidoUsuario,edadUsuario,dniUsuario,correoUsuario,provinciaUsuario,ciudadUsuario,nacionalidadUsuario)
+
+
+ 
+  if (formularioUsuario() == true) {
+    const menorEdad = document.getElementById("menorEdad");
+    const nacionalidad = document.getElementById("nacionalidad");
+    const edadMinima = 18;
+
+    if (usuario.edad < edadMinima) {
+      return (menorEdad.innerHTML = "Debes tener al menos 18 años para contratar el paquete de viaje.");
+    }
+      
+    
+
+    menorEdad.innerHTML = usuario.mayorEdad(edadMinima);
+    nacionalidad.innerHTML= usuario.nacionalidadPasajero();
+    const pasaporteDestino = JSON.parse(localStorage.getItem("paqueteViajeBusqueda"));
+    localStorage.setItem("datosUsuario", JSON.stringify(usuario));
+    setTimeout(() => (location.href = "pasajeUsuario.html"), 3000);
   }
 
-})
+  
 
+});
